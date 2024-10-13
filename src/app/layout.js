c6c06ecfc3aae41421513d29d43ae8077f565bb5
mjunaid249 <junaidch249249@gmail.com>
2024-10-13 17:09:11 +0500

    Added Record

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,7 @@ export default function RootLayout({ children }) {
         >
           <Navbar />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
